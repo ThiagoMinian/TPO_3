f31ada8118e2a961dc5aad6e89faf9f505784716
ThiagoMinian <101407767+ThiagoMinian@users.noreply.github.com>
2025-06-12 17:31:53 -0300

    test

---
+++ test/TestMA.java
@@ -0,0 +1,16 @@
+public static void testMatrizAdyacencia() {
+    IGrafo<Persona> grafo = new Grafo<>();
+
+    Persona ana = new Persona("Ana", 30);
+    Persona bruno = new Persona("Bruno", 25);
+    Persona carla = new Persona("Carla", 28);
+
+    grafo.agregarNodo(ana);
+    grafo.agregarNodo(bruno);
+    grafo.agregarNodo(carla);
+
+    grafo.agregarArista(ana, bruno);
+    grafo.agregarArista(bruno, carla);
+
+    grafo.mostrarMatrizAdyacencia();
+}
